c372c6d6e845c03b8a3123d9ef26616e5812a128
imarquez21 <32223683+imarquez21@users.noreply.github.com>
2017-10-04 12:55:17 +0200

    Create queue.h

---
+++ Practical2/queue.h
@@ -0,0 +1,31 @@
+
+#ifndef NSD_QUEUE_H
+#define NSD_QUEUE_H
+
+using namespace std;
+
+struct Node{
+    long val;
+    Node* next;
+};
+typedef struct Node Node;
+
+struct Queue{
+    Node* first;
+    Node* last;
+    long length;
+};
+typedef struct Queue Queue;
+
+Node* node_init(long val);
+void node_deinit(Node* node);
+Queue* queue_init();
+void queue_deinit(Queue* queue);
+long queue_pop(Queue* queue);
+void queue_push(Queue* queue, long node);
+long queue_first(Queue* queue);
+long queue_last(Queue* queue);
+void queue_print(Queue* queue);
+bool queue_is_empty(Queue* queue);
+
+#endif //NSD_QUEUE_H

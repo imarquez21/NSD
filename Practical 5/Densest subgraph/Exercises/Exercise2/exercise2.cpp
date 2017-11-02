75849c49896a520e8218468be6c876038f3c4265
Israel & Mutiah Hamid <32223683+imarquez21@users.noreply.github.com>
2017-11-02 22:05:44 +0100

    Create exercise2.cpp

---
+++ Practical 5/Densest subgraph/Exercises/Exercise2/exercise2.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include "adjacencylist.h"
+#include "heap.h"
+
+using namespace std;
+
+double compute_degree_density(long n, long m);
+double compute_edge_density(long n, long m);
+
+void store_file(char* filename, long** data, long length);
+long* core_decomposition(Graph* graph, long** data);
+long* densest_prefix(Graph* graph, long* core_decomposition);
+long max(long a, long b);
+
+// avg degree density = #edges_subgraph / #node_subgraph
+// avg edge density = 2*#edges_subgraph / ( #node_subgraph * (#node_subgraph -1) )
+
+// print size, avg degree density and avg edge density of the maximum
+
+int main(int argc, char** argv) {
+    Graph* graph;
+
+    cout << "NSD Course:" << endl << endl;
+    long start = time(NULL);
+
+    cout << "Loading graph..."<<endl;
+
+    if(argc > 1)
+        graph = graph_load_file(argv[1]);
+    else
+        graph = graph_load_file("graph3.txt");
+
+    cout << "Computing core decomposition..."<<endl;
+    long** data = new long*[graph->number_nodes];
+    for (int j = 0; j < graph->number_nodes; ++j) {
+        data[j] = new long[3];
+    }
+    long *core_dec = core_decomposition(graph, data);
+    cout << "Computing prefix..." << endl;
+    long *dens_prefixs = densest_prefix(graph, core_dec);
+
+    long max_densest_graph = 0;
+    double max_degree_density = 0.0;
+    double max_edge_density = 0.0;
+    double avg_degree_density = 0.0;
+    double avg_edge_density = 0.0;
+
+    cout << "Computing densities..." << endl;
+    for (int i = 0; i < graph->number_nodes; ++i) {
+        long subgraph_nodes = i+1;
+        double degree_density =  compute_degree_density(subgraph_nodes, dens_prefixs[i]);
+        double edge_density = compute_edge_density(subgraph_nodes, dens_prefixs[i]);
+        //cout << "Dense subgraph of size = " << subgraph_nodes << " nodes" << endl;
+        //cout << "\t Degree Density = " << degree_density << endl;
+        //cout << "\t Edge Density = " << edge_density << endl;
+
+        avg_degree_density += degree_density;
+        avg_edge_density += edge_density;
+
+        if(max_degree_density < degree_density){
+            max_degree_density = degree_density;
+            max_densest_graph = i;
+            max_edge_density = edge_density;
+        }
+    }
+
+    avg_degree_density /= graph->number_nodes;
+    avg_edge_density /= graph->number_nodes;
+
+    cout << endl;
+    cout << "Maximum size = " << max_densest_graph+1 << endl;
+    cout << "\twith degree density = " << max_degree_density << endl;
+    cout << "\twith edge density = " << max_edge_density << endl;
+    cout << "Avg degree density = " << avg_degree_density << endl;
+    cout << "Avg edge density = " << avg_edge_density << endl;
+
+    cout << "Storing file..." << endl;
+    store_file("output.dat", data, graph->number_nodes);
+
+    long end = time(NULL);
+    cout<<"\nTime required: "<<end-start<<" seconds"<<endl;
+
+    graph_deinit(graph);
+
+    return 0;
+}
+
+double compute_degree_density(long n, long m){
+    return (double)m/(double)n;
+}
+
+double compute_edge_density(long n, long m){
+    if(n > 1)
+        return (2.0 * (double) m) / (n * (n-1));
+    return 0.0;
+}
+
+long* core_decomposition(Graph* graph, long** data){
+    Heap* heap = heap_init(graph);
+    heap_restore(heap);
+
+    long* cd = new long[graph->number_nodes];
+
+    long n = graph->number_nodes-1;
+    long c = 0;
+    long index = 0;
+
+    while(! heap_is_empty(heap)){
+        //if(n % 100 == 0)
+        //    cout << n << endl;
+        long v = heap->heap[0].node;
+        //cout << "Removing " << v + heap->graph->offset << " ..." <<endl;
+        c = max(c, heap->heap[0].degree);
+
+        //heap_print(heap);
+        //cout << "After " << endl;
+
+        data[index][0] = graph->graph_degree[v];
+        data[index][1] = c;
+        data[index][2] = v;
+        index++;
+
+
+
+        heap_remove_node(heap, v);
+        cd[v] = n;
+
+        //heap_print(heap);
+
+        //cout << v+graph->offset << " with c= " << c << " in " << cd[v] << endl;
+        n--;
+    }
+
+    heap_deinit(heap);
+
+    return cd;
+}
+
+long* densest_prefix(Graph* graph, long* core_decomposition){
+    long* distributed = new long[graph->number_nodes];
+    long* cumulative = new long[graph->number_nodes];
+
+    memset(distributed, 0, sizeof(long)*graph->number_nodes);
+    memset(cumulative, 0, sizeof(long)*graph->number_nodes);
+
+    for (int i = 0; i < graph->number_nodes; ++i) {
+        for (int j = 0; j < graph->graph_degree[i]; ++j) {
+            long neighbour = graph->neighbours[graph->graph[i] + j];
+
+            if(i < neighbour){
+                //cout << "Studying "<<i+graph->offset<<" adding neighbour " << neighbour+graph->offset << " in position " << core_decomposition[neighbour] << " ";
+
+                if(core_decomposition[i] < core_decomposition[neighbour]){
+                    distributed[core_decomposition[neighbour]]++;
+                }else{
+                    distributed[core_decomposition[i]]++;
+                }
+
+               // cout << "distributed = " << distributed[core_decomposition[neighbour]] << endl;
+            }
+        }
+    }
+
+    //cout << "0 0" << endl;
+    for (int i = 1; i < graph->number_nodes; ++i) {
+        //cout << i << " " << distributed[i] << endl;
+        cumulative[i] = cumulative[i-1] + distributed[i];
+    }
+
+    delete[](distributed);
+
+    return cumulative;
+}
+
+long max(long a, long b){
+    return a>b ? a : b;
+}
+
+
+void store_file(char* filename, long** data, long length){
+    fstream output;
+    output.open(filename, ios::out);
+
+    for (int i = 0; i < length; ++i) {
+        output << data[i][0] << " " << data[i][1] << " " << data[i][2] << endl;
+    }
+
+    output.close();
+}
